2960a66d144172001a8596c7c52120ebe314ffed
Nandini N <nandinin9993@gmail.com>
2026-08-05 16:11:30 +0530

    Time: 488 ms (14.15%), Space: 31.4 MB (91%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -5,17 +5,11 @@ class Solution:
         while l<r:
             s=num[l]+num[r]
             if s==k:
-                # a,b=num[l],num[r]
-                # num.remove(a)
-                # num.remove(b)
-                # print(num)
                 c+=1
                 l+=1
                 r-=1
-                # l,r=0,len(num)-1
             elif s<k:
                 l+=1
             else:
-                # l+=1
                 r-=1    
         return c
