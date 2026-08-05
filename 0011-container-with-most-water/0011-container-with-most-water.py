4b5c8b2ef3a0eb541152b7e8051840515e12da89
Nandini N <nandinin9993@gmail.com>
2026-08-05 15:37:12 +0530

    Time: 585 ms (5.58%), Space: 30 MB (5.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -12,7 +12,3 @@ class Solution:
             else:
                 r-=1
         return maxi
-
-# Synced seamlessly with LeetHub Pro
-# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
